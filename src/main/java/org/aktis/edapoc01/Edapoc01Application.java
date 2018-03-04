72b33b85e12a1ef10d55b9b6edd14544381f8ca4
Keith Hazelton <khazelton@gmail.com>
2018-03-04 13:51:42 -0600

    Initial commit

---
+++ src/main/java/org/aktis/edapoc01/Edapoc01Application.java
@@ -0,0 +1,12 @@
+package org.aktis.edapoc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edapoc01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Edapoc01Application.class, args);
+    }
+}
---
+++ src/main/java/org/aktis/edapoc01/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.aktis.edapoc01;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Edapoc01Application.class);
+    }
+
+}
